81d30d2cd934abf1f3ac00282e886e958c3e4430
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2021-03-29 11:17:37 +0200

    Add files via upload

---
+++ Project2-Practice/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Project2-Practice
+//
+//  Created by Ayaka Mihara on 2021/03/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]() //新しい文字列の配列を保持するcountriesという新しいプロパティ
+    var score = 0 // 0に設定されたscoreの新しいプロパティ
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
+    }
+
+
+    // 画面に3つのランダムな旗の画像を表示するメソッド
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
+}
+
